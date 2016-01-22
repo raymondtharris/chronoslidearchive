cec28842ea20cf3dcea9895eb8fad01dec243fdf
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-21 20:56:50 -0800

    Started New Chronoslide
    First commit for new chronoslide app.

---
+++ ChronoSlide/ChronoSlide/Alarm.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm.swift
+//  ChronoSlide
+//
+//  Created by Tim Harris on 1/21/16.
+//  Copyright © 2016 Tim Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Alarm {
+    var alarmHour: Int
+    var alarmMinute: Int
+    var alarmState: Bool
+    var alarmNotification: UILocalNotification
+}
---
+++ ChronoSlide/ChronoSlide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChronoSlide
+//
+//  Created by Tim Harris on 1/21/16.
+//  Copyright © 2016 Tim Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
