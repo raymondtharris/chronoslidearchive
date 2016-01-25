93fc9d0951cdff5542139ab7f24bcece7b8285f0
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-24 22:58:03 -0800

    Deep Link to Settings
    added the deep link to the settings app for ChronoSlide.

--- ChronoSlide/ChronoSlide/ViewController.swift
+++ ChronoSlide/ChronoSlide/ViewController.swift
@@ -31,6 +31,10 @@ class ViewController: UIViewController {
 
 
 class AlarmTableViewController: UITableViewController {
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
+    
+    
+    
     var ChronoAlarms: [Alarm] = [Alarm]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +50,14 @@ class AlarmTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ChronoAlarms.count
     }
+    
+    
+    @IBAction func goToSettings(sender: AnyObject) {
+        if let chronoslideSettings = NSURL(string: UIApplicationOpenSettingsURLString){
+            UIApplication.sharedApplication().openURL(chronoslideSettings)
+        }
+    }
+    
     
 }
 
