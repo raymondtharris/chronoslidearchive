86ae23ce43c4a3ca80a7652485f1a4bc265e1a3e
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-24 16:48:20 -0800

    Settings and ViewControllers
    Added Settings Bundle.
    Added Navigation Controller, AlarmTableViewController, and
    NewAlarmViewController.

--- ChronoSlide/ChronoSlide/ViewController.swift
+++ ChronoSlide/ChronoSlide/ViewController.swift
@@ -29,3 +29,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var templabel: UILabel!
 }
 
+
+class AlarmTableViewController: UITableViewController {
+    var ChronoAlarms: [Alarm] = [Alarm]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let chronoAlarmCell = tableView.dequeueReusableCellWithIdentifier("", forIndexPath: indexPath)
+        let chronoAlarm = ChronoAlarms[indexPath.row]
+        chronoAlarmCell.textLabel?.text = chronoAlarm.alarmName
+        return chronoAlarmCell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ChronoAlarms.count
+    }
+    
+}
+
+class NewAlarmViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
