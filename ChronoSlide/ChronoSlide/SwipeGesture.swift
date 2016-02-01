61c656bf6fd98f67971b46f28e92a406d4f4b136
Tim Harris <raymondtimothyharris@gmail.com>
2016-01-31 21:21:10 -0800

    Custom Gesture
    Started to outline a new gesture that recognizes swipes better.

---
+++ ChronoSlide/ChronoSlide/SwipeGesture.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeGesture.swift
+//  ChronoSlide
+//
+//  Created by Tim Harris on 1/31/16.
+//  Copyright © 2016 Tim Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChronoSwipeGesture: UISwipeGestureRecognizer{
+    var velocity: CGVector?
+}
