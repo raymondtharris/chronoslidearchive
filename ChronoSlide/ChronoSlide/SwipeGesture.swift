7e38e26f54c95cb4494d360ea12fa92151d040a3
Tim Harris <raymondtimothyharris@gmail.com>
2016-04-22 20:02:01 -0700

    Building Gesture
    Starting to working on getting velocity for the gesture.

--- ChronoSlide/ChronoSlide/SwipeGesture.swift
+++ ChronoSlide/ChronoSlide/SwipeGesture.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import UIKit
+import UIKit.UIGestureRecognizerSubclass
 
-class ChronoSwipeGesture: UIControl{
+class ChronoSwipeGesture: UIGestureRecognizer{
     var velocity: CGVector?
     var force:CGVector?
     var startPosition:CGPoint?
@@ -17,9 +18,11 @@ class ChronoSwipeGesture: UIControl{
     var endPosition:CGPoint?
     var endTime: NSDate?
     
+    
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let aTouch = touches
-        startPosition  = aTouch.first?.locationInView(self)
+        startPosition  = aTouch.first?.locationInView(self.view)
         startTime = NSDate()
     }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -27,8 +30,14 @@ class ChronoSwipeGesture: UIControl{
     }
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let aTouch = touches
-        endPosition = aTouch.first?.locationInView(self)
+        endPosition = aTouch.first?.locationInView(self.view)
         
+        endTime = NSDate()
+        let deltaTime = endTime?.timeIntervalSinceDate(startTime!)
+        let deltaPositionX = endPosition!.x - startPosition!.x
+        let deltaPositionY = endPosition!.y - startPosition!.y
+        //let timeNumber = NSFloat
+        velocity = CGVectorMake(deltaPositionX/CGFloat(deltaTime!), deltaPositionY/CGFloat(deltaTime!))
         
     }
     override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
